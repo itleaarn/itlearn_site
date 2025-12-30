8eab03668e1610b32fd5f4b95c510165f66bdada
reza mohseni <155166979+rezaamohseni@users.noreply.github.com>
2025-12-30 23:09:42 +0330

    Initial configuration completed.

---
+++ root/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from django.contrib import admin
+from .views import home , about , contact
+
+app_name = 'root'
+urlpatterns = [
+    path('' , home , name = 'home'),
+    path('about/' , about , name='about'),
+    path('contact/' , contact , name='contact'),
+]
